4820fbe8fb2b889f90bb00106dfc12b2da601ce3
Dirge <fuaddabbad@gmail.com>
2026-05-02 10:09:47 +0700

    save

--- 4005/turret/turret.gd
+++ 4005/turret/turret.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var bullet_scene: PackedScene = preload("res://entities/bullets/bullet.tscn")
+@export var bullet_scene: PackedScene = preload("res://4005/bullet/bullet.tscn")
 @export var shots_per_second: float = 0.5
 
 @onready var muzzle: Marker2D = $Marker2D
