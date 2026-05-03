1eb71700ef25125ac99a880e487343fe2581b8b2
Dirge <fuaddabbad@gmail.com>
2026-05-03 18:35:50 +0700

    save

--- 4005/bullet/bullet.gd
+++ 4005/bullet/bullet.gd
@@ -2,14 +2,10 @@ extends Area2D
 
 @export var speed: float = 700.0
 
-@onready var screen_notifier: VisibleOnScreenNotifier2D = $VisibilityNotifier2D
-
-func _ready() -> void:
-	if not screen_notifier.screen_exited.is_connected(_on_visible_on_screen_notifier_2d_screen_exited):
-		screen_notifier.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited)
 
 func _physics_process(delta: float) -> void:
-	global_position += transform.x * speed * delta
+	position += transform.x * speed * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+
+func _on_visibility_notifier_2d_screen_exited() -> void:
 	queue_free()
--- 4005/turret/turret.gd
+++ 4005/turret/turret.gd
@@ -4,41 +4,12 @@ extends Node2D
 @export var shots_per_second: float = 0.5
 
 @onready var muzzle: Marker2D = $Marker2D
-@onready var _animated_sprite = $AnimatedSprite2D
 
-func _ready() -> void:
-	if _animated_sprite and not _animated_sprite.animation_finished.is_connected(_on_animated_sprite_animation_finished):
-		_animated_sprite.animation_finished.connect(_on_animated_sprite_animation_finished)
-
-var _cooldown: float = 0.0
 
 func _physics_process(delta: float) -> void:
-	_cooldown = max(_cooldown - delta, 0.0)
 	look_at(get_global_mouse_position())
 	_normalize_rotation()
-	_try_shoot()
 
 
 func _normalize_rotation() -> void:
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
-
-
-func _try_shoot() -> void:
-	if not Input.is_action_pressed("shoot"):
-		return
-	if _cooldown > 0.0:
-		return
-	if bullet_scene == null:
-		return
-
-	_animated_sprite.play("shoot")
-	var bullet: Area2D = bullet_scene.instantiate()
-	bullet.global_transform = muzzle.global_transform
-	get_tree().current_scene.add_child(bullet)
-
-	_cooldown = 1.0 / max(shots_per_second, 0.01)
-
-
-func _on_animated_sprite_animation_finished() -> void:
-	_animated_sprite.stop()
-	_animated_sprite.frame = 0
