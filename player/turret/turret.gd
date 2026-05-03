c50e59c1d4fa09b836d81a6d7408243d8a89e1e8
Dirge <fuaddabbad@gmail.com>
2026-05-03 19:52:10 +0700

    refactor | undo shooting animation

--- player/turret/turret.gd
+++ player/turret/turret.gd
@@ -1,22 +1,42 @@
 extends Node2D
 
 @export var Bullet: PackedScene = preload("res://player/bullet/bullet.tscn")
+@export var fire_cooldown: float = 6
 @onready var Muzzle: Marker2D = $Muzzle
 
+var _cooldown_timer: float = 0.0
 
-func _physics_process(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	_normalize_rotation()
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	_update_cooldown(delta)
+	if _can_fire():
+		_shoot()
 
 
 func _normalize_rotation() -> void:
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 
 
-func shoot():
-	var bullet_instance = Bullet.instantiate()
-	get_tree().root.add_child(bullet_instance)
-	bullet_instance.global_position = Muzzle.global_position
-	bullet_instance.rotation = rotation
+func _update_cooldown(delta: float) -> void:
+	if _cooldown_timer > 0.0:
+		_cooldown_timer = maxf(_cooldown_timer - delta, 0.0)
+
+
+func _can_fire() -> bool:
+	return Input.is_action_pressed("shoot") and _cooldown_timer == 0.0
+
+
+func _shoot() -> void:
+	if Bullet == null:
+		return
+	var bullet := Bullet.instantiate() as Area2D
+	if bullet == null:
+		return
+	bullet.global_transform = Muzzle.global_transform
+	var spawn_parent := get_tree().current_scene
+	if spawn_parent == null:
+		spawn_parent = get_tree().root
+	spawn_parent.add_child(bullet)
+	_cooldown_timer = fire_cooldown
