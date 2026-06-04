a046043a5eec353246bd0ee2f48da903a80c7fbe
Dirge <fuaddabbad@gmail.com>
2026-06-04 13:11:25 +0700

    feat | working gameover mechanic

--- scenes/windows/level_lost_window.gd
+++ scenes/windows/level_lost_window.gd
@@ -1,12 +1,20 @@
 @tool
 extends OverlaidWindow
 
-signal restart_pressed
-signal main_menu_pressed
+@export_file("*.tscn") var main_menu_scene_path : String
 
 func _ready():
 	if OS.has_feature("web"):
 		%ExitButton.hide()
+
+func get_main_menu_scene_path() -> String:
+	if main_menu_scene_path.is_empty():
+		return AppConfig.main_menu_scene_path
+	return main_menu_scene_path
+
+func _load_scene(scene_path: String) -> void:
+	_scene_tree.paused = false
+	SceneLoader.load_scene(scene_path)
 
 func _on_exit_button_pressed():
 	%ExitConfirmation.show()
@@ -15,12 +23,11 @@ func _on_main_menu_button_pressed():
 	%MainMenuConfirmation.show()
 
 func _on_close_button_pressed():
-	restart_pressed.emit()
+	SceneLoader.reload_current_scene()
 	close()
 
 func _on_main_menu_confirmation_confirmed():
-	main_menu_pressed.emit()
-	close()
+	_load_scene(get_main_menu_scene_path())
 
 func _on_exit_confirmation_confirmed():
 	get_tree().quit()
